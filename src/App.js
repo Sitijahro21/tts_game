2a8b7b1b46dbe895ed769b8e9619219502d4ae64
sitijahro21 <szmaulidiyah15@gmai.com>
2023-11-10 13:19:34 +0700

    update

--- src/App.js
+++ src/App.js
@@ -141,7 +141,7 @@ export default function App() {
 
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1>Iti Game</h1>
       <Crossword data={data}>
         <Grid>
           <Cell />
